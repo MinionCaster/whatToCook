7058d19b04537639030cf9750712337f8c618f7a
Psych00l <35641863+Psych00l@users.noreply.github.com>
2018-07-26 18:22:21 +0200

    started adding skeleton of an app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import './index.css';
+
+import Layout from './components/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Test
+        <Layout>
+          <p>Test</p>
+        </Layout>
       </div>
     );
   }
