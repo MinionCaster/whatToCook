3752730a4b30f296125c772b4c16b1a53dd70da4
Psych00l <35641863+Psych00l@users.noreply.github.com>
2018-07-27 20:07:02 +0200

    main container added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import './index.css';
 
 import Layout from './components/Layout/Layout';
+import WhatCook from './containers/WhatCook/WhatCook';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Layout>
-          <p>Test</p>
+          <WhatCook />
         </Layout>
       </div>
     );
---
+++ src/containers/WhatCook/WhatCook.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Aux';
+
+class WhatCook extends Component {
+    render () {
+        return (
+            <Aux>
+                <div>Ingredients</div>
+                <div>List of recipes</div>
+            </Aux>
+        );
+    }
+}
+
+export default WhatCook;
