31da90318b4a804c8be15bf805370fe5efe0bf2c
Psych00l <35641863+Psych00l@users.noreply.github.com>
2018-07-28 12:40:31 +0200

    added modal css

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
+import classes from './Modal.css';
 
 class Modal extends Component {
 
@@ -9,6 +10,14 @@ class Modal extends Component {
         return (
             <Aux>
                 <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+                <div
+                    className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}>
+                    {this.props.children}
+                </div>
             </Aux>
         )
     }
