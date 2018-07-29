e5f055df181b2c28e5310af42d577a2b67a105f9
Psych00l <35641863+Psych00l@users.noreply.github.com>
2018-07-29 10:31:21 +0200

    changed Modal a little

--- src/components/Recipe/SingleRecipe/SingleRecipe.js
+++ src/components/Recipe/SingleRecipe/SingleRecipe.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import SingleIngredient from './SingleIngredient/SingleIngredient';
 
+import classes from './SingleRecipe.css';
+
 const singleRecipe = props => {
 
     const ingredients = props.ingredients.map((el, index) => {
         return <SingleIngredient ing={el} key={index}/>
     })
     return (
-        <div>
+        <div className={classes.Container}>
             <p>{props.title}</p>
+            <img className = {classes.Image}src={props.image} alt={props.title}></img>
             <p>{props.author}</p>
-            <img src={props.image} alt={props.title}></img>
-            <p>{props.url}</p>
             <ul>
                 {ingredients}
             </ul>
+            <p>{props.url}</p>
         </div>
     )
 }
